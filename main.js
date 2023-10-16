36b86d6433576e23f4ace3b8a05283979b1bd63d
sanjeevchandru <chandrusanjeev5@gmail.com>
2023-10-16 09:55:44 +0530

    for-factorial-task-1

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+
+document.write("To check the number of factorial"+"<br>");
+document.write("---------------------------------"+"<br>")
+
+let fact=1;
+for(let i=1;i<=7;i++)
+{
+    fact=fact*i;
+}
+document.write("The factorial of 7 is:",fact);
